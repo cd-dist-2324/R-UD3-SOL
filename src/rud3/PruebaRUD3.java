2a5308aa8dc9678b3b8317d3344a31b6a257e8e3
cd-dist-2324 <cd.dist.2324@gmail.com>
2024-06-06 19:50:22 +0200

    INIT commit

---
+++ src/rud3/PruebaRUD3.java
@@ -0,0 +1,12 @@
+package rud3;
+public class PruebaRUD3 {
+    
+    public int prueba(int x, int y) {
+ 
+        int z = 0;
+        if((x>0) && (y>0)){
+            z=x;
+        }
+        return z;
+    }
+}
---
+++ tests/rud3/PruebaRUD3Test.java
@@ -0,0 +1,40 @@
+package rud3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class PruebaRUD3Test {
+
+    private static PruebaRUD3 app = null;
+
+    @BeforeAll
+    public static void init() {
+        app = new PruebaRUD3();
+
+    }
+    
+    @AfterAll
+    public static void end(){
+        app= null;
+    }
+
+    @Test
+    void testCamino1() {
+        int resultado = app.prueba(-1, 0);
+        assertEquals(0, resultado);
+    }
+
+    @Test
+    void testCamino2() {
+        int resultado = app.prueba(1, 0);
+        assertEquals(0, resultado);
+    }
+    @Test
+    void testCamino3() {
+        int resultado = app.prueba(1, 1);
+        assertEquals(1, resultado);
+    }
+}
